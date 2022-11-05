e8d7f83069e80c4e7a1b41d4a259dac0c2e1936d
Krosvick <josesand99v@gmail.com>
2022-11-04 23:34:14 -0300

    pocketbase tables setup

--- src/pocketst.ts
+++ src/pocketst.ts
@@ -1,10 +1,10 @@
 const PocketBase = require('pocketbase/cjs');
 const client = new PocketBase('http://127.0.0.1:8090');
 async function getAllRecords() {
-  const records = await client.records.getFullList("preguntas", 1,50);
-  console.log(records);
-  let resultData=records.items;
-  console.log(resultData);
+  const adminData = await client.admins.authViaEmail("email@gmail.com", "password");
+  const records = await client.records.getOne("preguntas", "hdtljd8anagvn6e" );
+  console.log(records.contenido);
 }
 
 getAllRecords();
+
