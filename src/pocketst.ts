a0bf083417ee1a0d93622d620025f48d0614d1f4
Krosvick <josesand99v@gmail.com>
2022-11-05 17:41:12 -0300

    login admin implementado

--- src/pocketst.ts
+++ src/pocketst.ts
@@ -1,4 +1,5 @@
 const PocketBase = require('pocketbase/cjs');
+const prompts = require('prompt-sync')();
 const client = new PocketBase('http://127.0.0.1:8090');
 
 async function getAllRecords() {
@@ -6,38 +7,69 @@ async function getAllRecords() {
   const records = await client.records.getOne("preguntas", "hdtljd8anagvn6e" );
   console.log(records.contenido);
 }
+async function agregarUsuario(){
+  console.clear();
+  console.log("Para ingresar un usuario se requiere de los siguientes campos");
+  console.log("Nombres, apellido paterno, apellido materno, correo, contraseña y rol");
+  console.log("Ingrese los datos del usuario");
+  let nombre = prompts("Nombres: ");
+  let apellidoPaterno = prompts("Apellido paterno: ");
+  let apellidoMaterno = prompts("Apellido materno: ");
+  let correo = prompts("Correo: ");
+  let contrasena = prompts("Contraseña: ");
+  let rol = prompts("Rol: ");
+  const user = await client.users.create({
+    names: nombre,
+    last_Name: apellidoPaterno,
+    mother_last_name: apellidoMaterno,
+    email: correo,
+    password: contrasena,
+    role: rol
+  });
+  console.log(user);
+}
 
-async function loginUsuario() {
+
+async function loginUsuario(){
   console.log("Ingrese su correo electronico"); 
-  var email = prompt("Correo: ");
+  var email = prompts("Correo: ");
   console.log("Ingrese su contraseña");
-  var password = prompt("Contraseña: ");
+  var password = prompts("Contraseña: ");
   const userData = await client.users.authViaEmail(email, password);
   console.clear();
   console.log("Bienvenido " + userData.name);
-};
+  if (userData.role == "Patient") {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 async function loginAdmin() {
   console.log("Ingrese su correo electronico");
-  var email = prompt("Correo: ");
+  var email = prompts("Correo: ");
   console.log("Ingrese su contraseña");
-  var password = prompt("Contraseña: ");
-  const adminData = await client.admins.authViaEmail(email, password);
-  console.clear();
-  console.log("Bienvenido " + adminData.name);
+  var password = prompts("Contraseña: ");
+  try{
+    const adminData = await client.admins.authViaEmail(email, password);
+    console.clear();
+    console.log("Bienvenido " + adminData.admin.id);
+  } catch (error) {
+    console.log("Error");
+  }
+  menuAdmin();
 };
 
 function login(){
-  //clear console
   console.clear();
   console.log("Bienvenido al sistema de encuestas");
   console.log("1) Iniciar sesion de usuario");
   console.log("2) Iniciar sesion de administrador");
   console.log("3) Salir");
-  var opcion = prompt("Ingrese una opcion: ");
+  var opcion = prompts("Ingrese una opcion: ");
   switch(opcion){
     case "1":
-      loginUsuario();
+      let tipo = loginUsuario();
       break;
     case "2":
       loginAdmin();
@@ -52,4 +84,49 @@ function login(){
   }
 
 }
+function menuUsuario(){
+  console.clear();
+  console.log("1) Ver encuestas disponibles");
+  console.log("2) Ver encuestas realizadas");
+  console.log("3) Salir");
+  var opcion = prompts("Ingrese una opcion: ");
+  switch(opcion){
+    case "1":
+      //verEncuestasDisponibles();
+      break;
+    case "2":
+      //verEncuestasRealizadas();
+      break;
+    case "3":
+      console.log("Gracias por usar el sistema de encuestas");
+      break;
+    default:
+      console.log("Opcion invalida");
+      menuUsuario();
+      break;
+  };
+}
+function menuPsicologo(){
+  console.clear();
+  console.log("1) ") 
+}
+function menuAdmin(){
+  console.clear();
+  console.log("1) Agregar Usuario"); 
+  console.log("2) Eliminar Usuario");
+  let opcion = prompts("Ingrese una opcion: ");
+  switch(opcion){
+    case "1":
+      agregarUsuario();
+      break;
+    case "2":
+      //eliminarUsuario();
+      break;
+    default:
+      console.log("Opcion invalida");
+      menuAdmin();
+      break;
+  }
+}
+login();
 
