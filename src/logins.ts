d671b32a717d98ad052c5c5e7639a6167ce63045
Krosvick <josesand99v@gmail.com>
2022-11-14 19:19:40 -0300

    test data fetch

--- src/logins.ts
+++ src/logins.ts
@@ -8,10 +8,10 @@ export async function loginPsicologo(){
   console.log("Ingrese su contraseña");
   var password = prompts("Contraseña: ");
   const res = await client.collection("psychologists").authWithPassword(email, password);
-  console.log(res)
   if(res){
-    console.log("Bienvenido");
+    console.log("Bienvenido " + res.record.names);
     menuPsicologo();
+    return;
   }else{
     console.log("Correo o contraseña invalidos");
   }
